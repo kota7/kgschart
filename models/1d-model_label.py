2ef552f065be17523ca82b8c94729980d6674f50
kota <kmori05@gmail.com>
2017-04-06 01:51:55 +0900

    two data combined

--- models/1d-model_label.py
+++ models/1d-model_label.py
@@ -13,13 +13,24 @@ proj_root = os.path.abspath(os.path.join(os.path.dirname( \
 datadir = os.path.join(proj_root, 'data/labels/')
 
 
-X = np.load(os.path.join(datadir, 'X.npy'))
-Y = np.load(os.path.join(datadir, 'Y.npy'))
+X1 = np.load(os.path.join(datadir, 'X-1.npy'))
+Y1 = np.load(os.path.join(datadir, 'Y-1.npy'))
 
+X2 = np.load(os.path.join(datadir, 'X-2.npy'))
+Y2 = np.load(os.path.join(datadir, 'Y-2.npy'))
+
+print(X1.shape, X2.shape)
+print(Y1.shape, Y2.shape)
+
+X = np.vstack((X1, X2))
+Y = np.concatenate((Y1, Y2))
+print(X.shape)
+print(Y.shape)
 
 X_train, X_test, Y_train, Y_test  = train_test_split(
         X, Y, test_size = 0.25, stratify = Y)
 
-pd.Series(Y_train).value_counts()
+print(pd.Series(Y_train).value_counts())
+print(pd.Series(Y_test).value_counts())
 
 
